0fa1ec22ce5d32dbe845392ddf10d616a216cfbf
Ekta Verma <eku4evr@gmail.com>
2014-09-17 21:11:19 +0530

    authentication via access token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :restrict_access
+  
+  #Use this for Using HTTP Basic Authentication
+  #http_basic_authenticate_with name: "admin", password: "secret"
+
   def index
   end
+
+  private
+  # def restrict_access
+  # 	api_key = ApiKey.find_by_access_token(params[:access_token])
+  # 	head :unauthorized unless api_key
+  # end
+
+  def restrict_access
+  	authenticate_or_request_with_http_token do |token, options|
+  		ApiKey.exists?(access_token: token)
+  	end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140917125949) do
+
+  create_table "api_keys", force: true do |t|
+    t.string   "access_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
